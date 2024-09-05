03269c1e6bbe77e1a717810464f7ae5df909e36f
Miklos24 <miklos.bowling@gmail.com>
2024-09-05 10:50:28 -0700

    Added experience tab

---
+++ components/gen/Experience.tsx
@@ -0,0 +1,19 @@
+export default function Experience() {
+  return (
+    <div className="content-container">
+      <h2>software engineer @ PayPal (2022 - present)</h2>
+      <p>
+        I work on{" "}
+        <a href="https://paypal.com/login">PayPal login</a>, where I build the
+        front end with <a href="https://reactjs.org">React</a> and&nbsp;
+        <a href="https://www.typescriptlang.org">TypeScript</a>,
+        leveraging&nbsp;
+        <a href="https://nextjs.org">Next.js</a>{" "}
+        for server-side rendering. I&#39;ve also had a hand in implementing a
+        {" "}
+        <a href="https://graphql.org">GraphQL</a>{" "}
+        midlayer and ensuring strong unit and functional test coverage.
+      </p>
+    </div>
+  );
+}
---
+++ content/markdowns/03_experience.md
@@ -0,0 +1,8 @@
+## software engineer @ PayPal (2022 - present)
+
+I work on [PayPal login](https://paypal.com/login), where I build the front end
+with [React](https://reactjs.org) and&nbsp;
+[TypeScript](https://www.typescriptlang.org), leveraging&nbsp;
+[Next.js](https://nextjs.org) for server-side rendering. I've also had a hand in
+implementing a [GraphQL](https://graphql.org) midlayer and ensuring strong unit
+and functional test coverage.
